f285fdd334088c17c183fa45268d77828f9d60d2
Kevin Ulberg <kevinulberg@hotmail.com>
2025-04-06 14:27:31 +1000

    added a 3rd plan

--- src/routes/Plans.svelte
+++ src/routes/Plans.svelte
@@ -6,6 +6,15 @@
 			planList: ['Custom website', 'Basic upkeep', 'SEO tweaks + email support']
 		},
 		{
+			header: 'Website only - $400',
+			title: 'What you get:',
+			planList: [
+				'Custom website',
+				'Editable source files (full control to tweak it)',
+				'Responsive design (looks great on all devices)'
+			]
+		},
+		{
 			header: 'Pro Plan - $299/month',
 			title: 'What you get:',
 			planList: [
@@ -22,15 +31,19 @@
 	<div class="grid place-content-center justify-evenly gap-20 xl:grid-cols-2">
 		{#each plans as plan}
 			<div
-				class=" mx-auto h-[600px] w-[600px] place-content-center rounded-2xl border-4 border-violet-400 bg-slate-950 p-5 duration-300 ease-in-out hover:cursor-pointer hover:bg-slate-800 hover:mix-blend-normal hover:shadow-amber-300"
+				class=" mx-auto h-[600px] w-[600px] place-content-center rounded-2xl border-4 border-violet-400 bg-slate-950 p-5 duration-300 ease-in-out hover:bg-slate-800 hover:mix-blend-normal hover:shadow-amber-300"
 			>
-				<h1 class="py-5 text-center underline">{plan.header}</h1>
-				<h3 class="py-4 text-center text-violet-400">{plan.title}</h3>
+				<h1 class="py-5 text-center text-2xl underline">{plan.header}</h1>
+				<h3 class="py-4 text-center text-xl text-violet-400">{plan.title}</h3>
 				<ul class="text-center">
 					{#each plan.planList as list}<li class="py-6 font-mono text-2xl font-semibold text-white">
 							{list}
 						</li>{/each}
 				</ul>
+				<button
+					class="rounded-xl border-4 border-amber-400 px-6 py-2 font-bold uppercase duration-300 ease-in-out hover:cursor-pointer hover:bg-amber-400 hover:text-black"
+					>Select</button
+				>
 			</div>
 		{/each}
 	</div>
