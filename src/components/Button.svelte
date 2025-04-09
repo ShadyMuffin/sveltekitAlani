9d81fbac0e196d76272102e742e367363146de95
Kevin Ulberg <kevinulberg@hotmail.com>
2025-04-09 16:30:57 +1000

    edit stripe

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -6,12 +6,14 @@
 
 	async function onclick() {
 		const stripe = await loadStripe(PUBLIC_STRIPE_KEY);
-		const response = fetch('/api/checkout', {
+
+		const response = await fetch('/api/checkout', {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			}
 		});
+		const { sessionId } = await response.json();
 	}
 </script>
 
@@ -19,5 +21,5 @@
 	{...props}
 	{onclick}
 	class="rounded-xl border-4 border-amber-400 px-6 py-2 font-extrabold uppercase duration-300 ease-in-out hover:cursor-pointer hover:bg-amber-400 hover:text-black"
-	>BUY NOW</button
+	>{@render children()}</button
 >
